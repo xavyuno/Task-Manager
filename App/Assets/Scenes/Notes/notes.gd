fde9a08a81d1dc9251eaad0064b046e823e8cd9e
xavyuno <xavierkaimgo@gmail.com>
2026-08-23 03:40:46 +0200

    display command

--- App/Assets/Scenes/Notes/notes.gd
+++ App/Assets/Scenes/Notes/notes.gd
@@ -24,6 +24,8 @@ var Options := [
 ]
 
 var Editing := false
+var Hovering := false
+var Displays = []
 
 func _ready() -> void :
 	initItem()
@@ -46,6 +48,8 @@ func _ready() -> void :
 	User.connect("PreviewNotes", Callable(self, "PreviewNotes"))
 	User.connect("AllFocusLost", Callable(self, "_on_notes_focus_exited"))
 	User.connect("RecieveByID", Callable(self, "RecieveByID"))
+	Commands.connect("CreateDisplay", Callable(self, "CreateDisplay"))
+	Commands.connect("CommandHovered", Callable(self, "CommandHovered"))
 
 func MouseEntered():
 	if RichText.get_v_scroll_bar().max_value - RichText.size.y >= 0:
@@ -64,6 +68,7 @@ func ChangeFontSize(value : int):
 	RichText.add_theme_font_size_override("italics_font_size", value)
 	RichText.add_theme_font_size_override("mono_font_size", value)
 	RichText.add_theme_font_size_override("normal_font_size", value)
+	$ScrollContainer/Notes.add_theme_font_size_override("font_size", value)
 	Data["FontSize"] = value
 
 func _process(delta: float) -> void :
@@ -75,6 +80,9 @@ func _process(delta: float) -> void :
 	
 	if Input.is_action_just_pressed("Bold") and Input.is_action_pressed("Command") and Editing:
 		RichTextUpdate("b")
+	if Input.is_action_just_pressed("Italic") and Input.is_action_pressed("Command") and Editing:
+		RichTextUpdate("i")
+	
 
 func EditNotes(GrabFocus = true):
 	RichText.visible = !Editing
@@ -87,6 +95,26 @@ func _on_notes_focus_exited() -> void:
 		UpdateText()
 		Editing = false
 		EditNotes(false)
+
+func CommandHovered(Hovered : bool, cmd : String):
+	if !Hovered:
+		pass
+	else:
+		cmd = cmd.replace("display/", "")
+		CreateDisplay(cmd)
+
+func CreateDisplay(imagePath):
+	print(imagePath)
+	var texture = ImageTexture.create_from_image(Image.load_from_file(ProjectSettings.globalize_path(imagePath)))
+	var display = TextureRect.new()
+	display.texture = texture
+	get_tree().current_scene.add_child(display)
+	display.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	if !(display in Displays):
+		Displays.append(display)
+	display.offset_transform_enabled = true
+	display.offset_transform_position = get_global_mouse_position()
+	#display.size = Vector2(500, 500)
 
 func UpdateText():
 	var Temptext : String = Data["Note"]
@@ -124,17 +152,25 @@ func _on_text_meta_clicked(meta ) -> void:
 	if !Commands.CheckCommand(meta):
 		OS.shell_open(str(meta))
 
-func RichTextUpdate(text, additonal = ""):
+func RichTextUpdate(text):
 	if NotesText.get_selected_text():
 		var selText = NotesText.get_selected_text()
-		NotesText.insert_text("[" + text + additonal + "]", NotesText.get_caret_line(), NotesText.get_caret_column(), true, true)
-		NotesText.insert_text("[/" + text + "]", NotesText.get_caret_line(), NotesText.get_caret_column() + selText.length(), false, false)
+		NotesText.text = NotesText.text.replace(selText, "[" + text + "]" + selText + "[/" + text + "]")
 	else :
 		NotesText.insert_text("[" + text + "]", NotesText.get_caret_line(), NotesText.get_caret_column(), true, true)
 		NotesText.insert_text("[/" + text + "]", NotesText.get_caret_line(), NotesText.get_caret_column(), false, false)
 
+
 func _on_text_meta_hover_started(meta: Variant) -> void:
-	RichText.tooltip_text = str(meta)
+	Hovering = true
+	if !Commands.CheckCommand(str(meta).to_lower().split('/')):
+		RichText.tooltip_text = str(meta)
+	else:
+		CreateDisplay(str(meta).replace("display/", ""))
 
 func _on_text_meta_hover_ended(meta: Variant) -> void:
+	Hovering = false
 	RichText.tooltip_text = ""
+	while Displays.size() >= 1:
+		Displays[0].queue_free()
+		Displays.pop_front()
